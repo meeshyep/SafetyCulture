a66bb546bd9962ad1cb54e6884d65e80779b4ce9
Michelle Epstein <epstein.michelle@gmail.com>
2017-05-05 07:14:58 +0000

    Light can change from Green to Red, with 30 seconds in Yellow

--- lib/traffic_light.rb
+++ lib/traffic_light.rb
@@ -2,9 +2,21 @@ class TrafficLight
     
     attr_accessor :id, :direction, :color
     
-    def initialize(id, direction)
+    def initialize(id, direction, color)
         @id = id
         @direction = direction
         @color = 'Red'
     end 
+    
+    def change_color
+        if @color =='Red'
+            @color = 'Green'
+        elsif @color == 'Green'
+            @color = 'Yellow'
+            puts @color
+            sleep(30) 
+            @color = 'Red'
+            puts @color
+        end 
+    end
 end 
--- spec/traffic_light_spec.rb
+++ spec/traffic_light_spec.rb
@@ -2,10 +2,16 @@ require 'traffic_light'
 
 describe TrafficLight do 
     
-    subject(:traffic_light) {described_class.new(1, "N")}
+    subject(:traffic_light) {described_class.new(1, "N", 'Red')}
 
     it 'is initialized with an id, direction and light color' do
         expect(subject.id).to eq 1
         expect(subject.direction).to eq 'N'
+        expect(subject.color).to eq 'Red'
     end 
+    
+    it 'can change colors' do 
+        subject.change_color
+        expect(subject.color).to eq 'Green'
+    end
 end 
